24bf281f6f1bbecc7e39108ab899bb551394d1d8
Manuchar Chachanidze <manucharch@gmail.com>
2025-01-24 16:23:54 +0400

    created some routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,37 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { MainComponent } from './home/main/main.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MainComponent } from './pages/home/main/main.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
   {
     path: '',
     component: HomeComponent,
-    children: [{ path: 'main', component: MainComponent }],
+    children: [
+      {
+        path: '',
+        redirectTo: 'main',
+        pathMatch: 'full',
+      },
+      {
+        path: 'main',
+        component: MainComponent,
+      },
+      {
+        path: 'tictoc',
+        loadComponent: () =>
+          import('./pages/tictoc/tictoc.component').then(
+            (m) => m.TictocComponent
+          ),
+      },
+      {
+        path: 'picker',
+        loadComponent: () =>
+          import('./pages/picker/picker.component').then(
+            (m) => m.PickerComponent
+          ),
+      },
+    ],
   },
-  {
-    path: '**',
-    redirectTo: '',
-  },
+
+  { path: '**', redirectTo: '' },
 ];
---
+++ src/app/pages/tictoc/tictoc.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tictoc',
+  imports: [],
+  templateUrl: './tictoc.component.html',
+  styleUrl: './tictoc.component.scss'
+})
+export class TictocComponent {
+
+}
