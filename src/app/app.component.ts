473962bf9fa5d44238f948e536375a115fb37df5
Manuchar Chachanidze <manucharch@gmail.com>
2025-01-27 16:07:13 +0400

    ngx-translate is ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, TranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'MyWork';
+
+  constructor(private translate: TranslateService) {
+    this.translate.addLangs(['ge', 'en']);
+    this.translate.setDefaultLang('ge');
+    this.translate.use('ge');
+  }
 }
