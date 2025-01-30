57a1bfefd703a00acceede39e5a25e32040bce21
Manuchar Chachanidze <manucharch@gmail.com>
2025-01-30 14:45:00 +0400

    small corrections

--- src/app/pages/home/components/main/main.component.ts
+++ src/app/pages/home/components/main/main.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { GoToBtnComponent } from '../../../../components/go-to-btn/go-to-btn.component';
 
 @Component({
   selector: 'app-main',
-  imports: [RouterLink, TranslateModule, GoToBtnComponent],
+  imports: [TranslateModule, GoToBtnComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
