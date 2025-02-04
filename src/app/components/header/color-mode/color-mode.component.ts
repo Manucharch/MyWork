049b349464f8760b2effd49b497570eb5e591dd7
Manuchar Chachanidze <manucharch@gmail.com>
2025-02-04 09:59:51 +0400

    working on color modes

--- src/app/components/header/color-mode/color-mode.component.ts
+++ src/app/components/header/color-mode/color-mode.component.ts
@@ -9,24 +9,22 @@ import { BehaviorSubject } from 'rxjs';
   styleUrl: './color-mode.component.scss',
 })
 export class ColorModeComponent {
-  private currentTheme = new BehaviorSubject<'light' | 'dark'>('light');
+  private currentTheme = new BehaviorSubject<string>('light');
   currentTheme$ = this.currentTheme.asObservable();
 
   mode: string = 'light';
 
   constructor() {
-    const savedTheme =
-      (localStorage.getItem('theme') as 'light' | 'dark') || 'light';
+    const savedTheme = localStorage.getItem('theme') || 'light';
     this.setTheme(savedTheme);
   }
 
   toggleTheme() {
     this.mode = this.mode == 'light' ? 'dark' : 'light';
-    const newTheme = this.currentTheme.value === 'light' ? 'dark' : 'light';
-    this.setTheme(newTheme);
+    this.setTheme(this.mode);
   }
 
-  private setTheme(theme: 'light' | 'dark') {
+  private setTheme(theme: string) {
     this.currentTheme.next(theme);
     document.documentElement.setAttribute('data-theme', theme);
     localStorage.setItem('theme', theme);
